67804673b39874fe4a9ede28bcba957a0428f45b
april <aprils09@outlook.com>
2025-05-13 22:00:13 +0800

    [app-builder] 修改查询历史发布版本接口，新增恢复应用到指定版本接口

--- app-builder/jane/plugins/aipp-plugin/src/main/java/modelengine/fit/jober/aipp/service/AppBuilderAppService.java
+++ app-builder/jane/plugins/aipp-plugin/src/main/java/modelengine/fit/jober/aipp/service/AppBuilderAppService.java
@@ -157,10 +157,10 @@ public interface AppBuilderAppService {
      * @param limit 表示获取数据的最大个数的 {@code int}。
      * @param appId 表示应用唯一标识的 {@link String}。
      * @param context 表示操作上下文的 {@link OperationContext}。
-     * @return 获取到的历史版本信息集合的 {@link List}{@code <}{@link PublishedAppResDto}{@code >}。
+     * @return 获取到的历史版本信息集合的 {@link RangedResultSet}{@code <}{@link PublishedAppResDto}{@code >}。
      */
     @Genericable(id = "modelengine.fit.jober.aipp.service.app.recent.published")
-    List<PublishedAppResDto> recentPublished(AppQueryCondition cond, long offset, int limit, String appId,
+    RangedResultSet<AppBuilderAppDto> recentPublished(AppQueryCondition cond, long offset, int limit, String appId,
             OperationContext context);
 
     /**
@@ -231,4 +231,15 @@ public interface AppBuilderAppService {
      */
     @Genericable(id = "modelengine.fit.jober.aipp.service.app.deleteTemplate")
     void deleteTemplate(String templateId, OperationContext context);
+
+    /**
+     * 恢复应用到指定历史版本。
+     *
+     * @param appId 表示应用 id 的 {@link String}。
+     * @param resetId 表示指定历史版本 id 的 {@link String}。
+     * @param context 表示接口操作上下文的 {@link OperationContext}。
+     * @return 表示恢复应用完成后应用详情的 {@link AppBuilderAppDto}。
+     */
+    @Genericable(id = "modelengine.fit.jober.aipp.service.app.reset")
+    AppBuilderAppDto resetApp(String appId, String resetId, OperationContext context);
 }
